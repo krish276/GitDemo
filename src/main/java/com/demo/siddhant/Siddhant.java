75b042cdc15b74383b63a743e988993235f22e1b
Saurav Sanyal <sanyal.s271@gmail.com>
2019-07-12 11:50:37 -0700

    Sidd Demo

---
+++ src/main/java/com/demo/siddhant/Siddhant.java
@@ -0,0 +1,16 @@
+package com.demo.siddhant;
+
+public class Siddhant {
+	
+	public String sentence(String sentence) {
+		return sentence;
+	}
+	
+	
+public void siddDemo() {
+
+	
+	System.out.println("Siddhant is a good boy");
+}
+
+}
